0a12cc0b1837cb2cede75dbb743334f2dc7279d9
tinyRipple <zheng.dong.hui@foxmail.com>
2021-08-14 21:55:26 +0800

    2021-08-14

---
+++ FrontendStudy/Vue.js/Vue2.js/014-axios/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App'
+import axios from 'axios'
+
+Vue.config.productionTip = false
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+
+axios({
+  url: 'http://123.207.32.32:8000/home/multidata',
+  method: 'get'
+}).then(res => {
+  console.log(res);
+})
+
