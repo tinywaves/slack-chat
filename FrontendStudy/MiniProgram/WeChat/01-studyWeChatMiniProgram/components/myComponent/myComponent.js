4ee39d325e39aa91288b64c6a1e0e4cab0411e98
tinyRipple <zheng.dong.hui@foxmail.com>
2021-08-23 21:36:52 +0800

    MiniProgram-WeChat-data/methods/properties

--- FrontendStudy/MiniProgram/WeChat/01-studyWeChatMiniProgram/components/myComponent/myComponent.js
+++ FrontendStudy/MiniProgram/WeChat/01-studyWeChatMiniProgram/components/myComponent/myComponent.js
@@ -4,23 +4,47 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    max: Number,
+    min: {
+      type: Number,
+      value: 10
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    count: 0
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    addCount() {
+      if (this.data.count >= this.properties.max) {
+        return
+      }
+      this.setData({
+        count: this.data.count + 1,
+        max: this.properties.max + 1
+      })
+      this._showCount()
+    },
+    showInfo() {
+      console.log(this.data);
+      console.log(this.properties);
+      console.log(this.data === this.properties);
+    },
+    _showCount() {
+      wx.showToast({
+        title: '当前的count值为：' + this.data.count,
+        icon: 'none'
+      })
+    }
   },
   options: {
-    styleIsolation: isolated
+    styleIsolation: 'isolated'
   }
 })
