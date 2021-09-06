9a2066c6e8a72715030f1739054ed510d1343b84
tinyRipple <zheng.dong.hui@foxmail.com>
2021-09-06 23:58:25 +0800

    TypeScript

---
+++ FrontendStudy/Vue.js/Vue3.js/15-TypeScript/01-HelloTypeScript.ts
@@ -0,0 +1,7 @@
+const message: String = 'Hello TypeScript'
+console.log(message);
+
+function foo(payload: String) {
+  console.log(payload.length);
+}
+foo('Hello TypeScript')
---
+++ FrontendStudy/Vue.js/Vue3.js/15-TypeScript/webpackForTs/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
+  devServer: {
+
+  }
+}
