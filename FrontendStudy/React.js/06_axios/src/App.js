8a3fa929baeb3c226bd5782119ce4faecefb2c96
tinyRipple <zheng.dong.hui@foxmail.com>
2021-10-01 16:42:09 +0800

    ReactComponent-2021/10/01-axios/transition

--- FrontendStudy/React.js/06_axios/src/App.js
+++ FrontendStudy/React.js/06_axios/src/App.js
@@ -1,11 +1,132 @@
 import React, { PureComponent } from 'react'
+import axios from 'axios'
+
+import instance from './service/request'
 
 export default class App extends PureComponent {
-  render() {
-    return (
-      <div>
-        AAAA
-      </div>
+  // async componentDidMount() {
+  //   // 基于Promise
+  //   // GET请求
+  //   axios({
+  //     baseURL: 'http://httpbin.org',
+  //     url: '/get',
+  //     params: {
+  //       name: 'tinyRipple',
+  //       age: 20
+  //     },
+  //     method: 'GET',
+  //     timeout: 5000
+  //   })
+  //     .then(res => {
+  //       console.log(res)
+  //     })
+  //     .catch(error => {
+  //       console.log(error)
+  //     })
+  //   // POST请求
+  //   axios({
+  //     url: 'http://httpbin.org/post',
+  //     data: {
+  //       name: 'tinyRipple',
+  //       age: 20
+  //     },
+  //     method: 'POST'
+  //   })
+  //     .then(res => {
+  //       console.log(res)
+  //     })
+  //     .catch(error => {
+  //       console.log(error)
+  //     })
+  //   axios
+  //     .get('http://httpbin.org/get', {
+  //       params: {
+  //         name: 'tinyRipple',
+  //         age: 20
+  //       }
+  //     })
+  //     .then(console.log)
+  //   axios
+  //     .post('http://httpbin.org/post', {
+  //       data: {
+  //         name: 'tinyRipple',
+  //         age: 20
+  //       }
+  //     })
+  //     .then(console.log)
+
+  //   // 基于await-async
+  //   try {
+  //     const res = await axios.post('http://httpbin.org/post', {
+  //       data: {
+  //         name: 'tinyRipple',
+  //         age: 20
+  //       }
+  //     })
+  //     console.log('await-async', res)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  // }
+  componentDidMount() {
+    // 拦截器
+    // 请求拦截
+    axios.interceptors.request.use(
+      config => {
+        console.log('请求被拦截')
+        return config
+      },
+      error => {}
     )
+    // 响应拦截
+    axios.interceptors.response.use(
+      res => {
+        return res.data
+      },
+      error => {
+        if (error && error.response) {
+          switch (error.response.status) {
+            case 400:
+              console.log('请求错误')
+              break
+            case 401:
+              console.log('未授权访问')
+              break
+            default:
+              break
+          }
+        }
+        return error
+      }
+    )
+    // axios
+    //   .get('http://httpbin.org/get', {
+    //     params: {
+    //       name: 'tinyRipple',
+    //       age: 20
+    //     }
+    //   })
+    //   .then(res => {
+    //     console.log(res)
+    //   })
+    //   .catch(error => {
+    //     console.log(error)
+    //   })
+    instance
+      .get('http://httpbin.org/get', {
+        params: {
+          name: 'tinyRipple',
+          age: 20
+        }
+      })
+      .then(res => {
+        console.log(res)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+  render() {
+    return <div>App</div>
   }
 }
--- FrontendStudy/React.js/07_transition/src/index.js
+++ FrontendStudy/React.js/07_transition/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-// import axios from 'axios'
 
 import App from './App'
-
-// axios.defaults.baseURL = 'http://httpbin.org'
-// axios.defaults.timeout = 5000
 
 ReactDOM.render(<App />, document.getElementById('root'))
