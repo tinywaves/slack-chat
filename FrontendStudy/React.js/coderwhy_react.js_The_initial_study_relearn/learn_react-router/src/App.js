489e21d73ffb9f941c648fb9189da670110e1700
tinyRipple <zheng.dong.hui@foxmail.com>
2022-02-08 23:10:55 +0800

    react-router/redux/typescript

--- FrontendStudy/React.js/coderwhy_react.js_The_initial_study_relearn/learn_react-router/src/App.js
+++ FrontendStudy/React.js/coderwhy_react.js_The_initial_study_relearn/learn_react-router/src/App.js
@@ -1,20 +1,7 @@
-import React, { PureComponent } from 'react'
-import { HashRouter, Link, Route } from 'react-router-dom'
-import Home from './pages/home'
-import About from './pages/about'
+import React, { PureComponent } from 'react';
 
 export default class App extends PureComponent {
   render() {
-    return (
-      <div>
-        <HashRouter>
-          <Link to="/home">HOme</Link>
-          <Link to="/about">Ablout</Link>
-
-          <Route path="/home" component={Home} />
-          <Route path="/about" component={About} />
-        </HashRouter>
-      </div>
-    )
+    return <div>APP</div>;
   }
 }
