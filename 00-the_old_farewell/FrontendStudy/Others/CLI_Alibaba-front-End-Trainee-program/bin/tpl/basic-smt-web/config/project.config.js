3b2d7a832091e7ff953047cbaabcb2d7a5dec66d
tinyRipple <isdhzh@gmail.com>
2022-09-09 17:05:58 +0800

    remove sensitive information

--- 00-the_old_farewell/FrontendStudy/Others/CLI_Alibaba-front-End-Trainee-program/bin/tpl/basic-smt-web/config/project.config.js
+++ 00-the_old_farewell/FrontendStudy/Others/CLI_Alibaba-front-End-Trainee-program/bin/tpl/basic-smt-web/config/project.config.js
@@ -4,18 +4,18 @@ module.exports = {
 	// 接口环境
 	apiEnv: {
 		// 生产环境
-		API_BASE_URL_PROD: '//smt-app.pingan.com.cn/smtapp',
+		API_BASE_URL_PROD: '',
 		// 预发布环境
-		API_BASE_URL_PRE: '//smt-app.pingan.com.cn/smtapp',
+		API_BASE_URL_PRE: '',
 		// STG1 开发联调环境
-		API_BASE_URL_STG1: '//smt-stg1.yun.city.pingan.com/api',
+		API_BASE_URL_STG1: '',
 		// STG2 测试环境
-		API_BASE_URL_STG2: '//basesmt-caas.yun.city.pingan.com/api',
+		API_BASE_URL_STG2: '',
 		// 后端开发本地环境 需要与开发确认
-		API_BASE_URL_LOCAL: '//10.118.142.80:8199/api/platform',
+		API_BASE_URL_LOCAL: '',
 		// MOCK 地址，详情参照 yapi 文档
-		API_BASE_URL_MOCK: '//30.23.12.175:3000/mock/39'
+		API_BASE_URL_MOCK: ''
 	}
 	// 自定义发布地址
 	// deployEnv: ''
-}
+};
