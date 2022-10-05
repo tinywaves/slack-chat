637d0168d9c0b6c2b60ee231a0e7ddc47a1f1e8e
tinyRipple <isdhzh@gmail.com>
2022-10-05 20:42:40 +0800

    <ts-react> hook finished

--- ts-react/src/App.tsx
+++ ts-react/src/App.tsx
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 
 import Hello from './components/Hello';
 import IncrementButton from './components/IncrementButton';
 import MouseTracker from './components/MouseTracker';
 
-import withLoader from './components/withLoader';
+// import withLoader from './components/withLoader';
 
-import useMousePosition from './hooks/useMousePosition';
+// import useMousePosition from './hooks/useMousePosition';
 import useURLLoader from './hooks/useURLLoader';
 
 interface IDodImageData {
@@ -14,17 +14,32 @@ interface IDodImageData {
   status: string;
 }
 
-const DogImage: React.FC<{ data: IDodImageData }> = ({ data }) => {
-  return (
-    <>
-      <h2>status: {data.status}</h2>
-      <img src={data.message} alt="dog" />
-    </>
-  );
+interface ITheme {
+  [key: string]: { color: string; background: string };
+}
+const theme: ITheme = {
+  light: {
+    color: '#000',
+    background: '#eee'
+  },
+  dark: {
+    color: '#fff',
+    background: '#000'
+  }
 };
+export const ThemeContext = createContext(theme.light);
+
+// const DogImage: React.FC<{ data: IDodImageData }> = ({ data }) => {
+//   return (
+//     <>
+//       <h2>status: {data.status}</h2>
+//       <img src={data.message} alt="dog" />
+//     </>
+//   );
+// };
 
 const App: React.FC = () => {
-  const position = useMousePosition();
+  // const position = useMousePosition();
   const [flag, setFlag] = useState(false);
 
   // const Dog = withLoader(DogImage, 'https://dog.ceo/api/breeds/image/random');
@@ -35,23 +50,25 @@ const App: React.FC = () => {
   const result = data as IDodImageData;
 
   return (
-    <div style={{ textAlign: 'center' }}>
-      <Hello message="Hello, React.js + TypeScript" />
-      <IncrementButton number={0} condition={false} />
-      <MouseTracker />
-      {/* <p>
+    <ThemeContext.Provider value={theme.light}>
+      <div style={{ textAlign: 'center' }}>
+        <Hello message="Hello, React.js + TypeScript" />
+        <IncrementButton number={0} condition={false} />
+        <MouseTracker />
+        {/* <p>
           X: {position.pointerX}, Y: {position.pointerY}
         </p> */}
-      {/* <Dog /> */}
-      <button onClick={() => setFlag(!flag)} style={{ display: 'block' }}>
-        change flag
-      </button>
-      {loading ? (
-        <p>dog image is loading···</p>
-      ) : (
-        <img src={result && result.message} alt={result && result.status} />
-      )}
-    </div>
+        {/* <Dog /> */}
+        <button onClick={() => setFlag(!flag)} style={{ display: 'block' }}>
+          change flag
+        </button>
+        {loading ? (
+          <p>dog image is loading···</p>
+        ) : (
+          <img src={result && result.message} alt={result && result.status} />
+        )}
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
--- ts-react/src/components/Hello.tsx
+++ ts-react/src/components/Hello.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { ThemeContext } from '../App';
 
 interface IHelloProps {
   message?: string;
 }
 
 const Hello: React.FC<IHelloProps> = props => {
-  return <h2>{props.message}</h2>;
+  const theme = useContext(ThemeContext);
+
+  const style = {
+    color: theme.color,
+    backgroundColor: theme.background
+  };
+
+  return (
+    <>
+      <h2>{props.message}</h2>
+      <p style={style}>
+        {theme.color} + {theme.background}
+      </p>
+    </>
+  );
 };
 
 Hello.defaultProps = {
