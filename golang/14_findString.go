27616cbdc41fd2e7941e143434b0c14a70c14fbe
Denver Cheng <isdhzh@gmail.com>
2023-01-10 20:31:20 +0800

    <go> fix

--- golang/14_findString.go
+++ golang/14_findString.go
@@ -5,24 +5,29 @@ import "fmt"
 // 寻找最长不含有重复字符的字串
 
 func lengthOfNonRepeatingSubString(s string) int {
-	lastOccurred := make(map[byte]int)
-	start := 0
-	maxlength := 0
-	for i, character := range []byte(s) {
-		if lastI, ok := lastOccurred[character]; ok && lastI >= start {
-			start = lastI + 1
-		}
-		if i-start+1 > maxlength {
-			maxlength = i - start + 1
-		}
-		lastOccurred[character] = i
-	}
+    lastOccurred := make(map[byte]int)
+    start := 0
+    maxlength := 0
+    for i, character := range []byte(s) {
+        if lastI, ok := lastOccurred[character]; ok && lastI >= start {
+            start = lastI + 1
+        }
+        if i-start+1 > maxlength {
+            maxlength = i - start + 1
+        }
+        lastOccurred[character] = i
+    }
 
-	return maxlength
+    return maxlength
 }
 
 func main() {
-	fmt.Println(lengthOfNonRepeatingSubString("abcabcbb"))
-	fmt.Println(lengthOfNonRepeatingSubString("bbbbb"))
-	fmt.Println(lengthOfNonRepeatingSubString("pwwkew"))
+    // ascii -> ok
+    fmt.Println(lengthOfNonRepeatingSubString("abcabcbb"))
+    fmt.Println(lengthOfNonRepeatingSubString("bbbbb"))
+    fmt.Println(lengthOfNonRepeatingSubString("pwwkew"))
+    // chinese -> fail
+    fmt.Println(lengthOfNonRepeatingSubString("一二三四五"))
+    fmt.Println(lengthOfNonRepeatingSubString("Go语言"))
+    fmt.Println(lengthOfNonRepeatingSubString("测试算法题"))
 }
