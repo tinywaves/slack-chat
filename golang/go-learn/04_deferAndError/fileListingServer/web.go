04dddba8458f84b160777ddbcaa5e78b5434541c
Denver Cheng <isdhzh@gmail.com>
2023-01-15 22:04:40 +0800

    <go> error

--- golang/go-learn/04_deferAndError/fileListingServer/web.go
+++ golang/go-learn/04_deferAndError/fileListingServer/web.go
@@ -1,31 +1,36 @@
 package main
 
 import (
-	"io"
-	"net/http"
-	"os"
+    "04_deferAndError/fileListingServer/filelisting"
+    "net/http"
+    "os"
 )
 
+type appErrorHandler func(writer http.ResponseWriter, request *http.Request) error
+
+func appErrorWrapper(handler appErrorHandler) func(http.ResponseWriter, *http.Request) {
+    return func(writer http.ResponseWriter, request *http.Request) {
+        err := handler(writer, request)
+        if err != nil {
+            code := http.StatusOK
+            switch {
+            case os.IsNotExist(err):
+                code = http.StatusNotFound
+            case os.IsPermission(err):
+                code = http.StatusForbidden
+            default:
+                code = http.StatusInternalServerError
+            }
+            http.Error(writer, http.StatusText(code), code)
+        }
+    }
+}
+
 func main() {
-	http.HandleFunc("/list/", func(writer http.ResponseWriter, request *http.Request) {
-		path := request.URL.Path[len("/list/"):]
-		file, err := os.Open(path)
-		if err != nil {
-			http.Error(writer, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer file.Close()
+    http.HandleFunc("/list/", appErrorWrapper(filelisting.HandleFileList))
 
-		bytes, err := io.ReadAll(file)
-		if err != nil {
-			panic(err)
-		}
-
-		writer.Write(bytes)
-	})
-
-	err := http.ListenAndServe(":8888", nil)
-	if err != nil {
-		panic(err)
-	}
+    err := http.ListenAndServe(":8888", nil)
+    if err != nil {
+        panic(err)
+    }
 }
