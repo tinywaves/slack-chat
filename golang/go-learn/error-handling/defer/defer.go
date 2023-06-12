6523241bdce63ad206555d8609877ad5f7b933ef
Denver Zheng <isdhzh@outlook.com>
2023-06-12 22:49:07 +0800

    <go> defer2

--- golang/go-learn/error-handling/defer/defer.go
+++ golang/go-learn/error-handling/defer/defer.go
@@ -6,6 +6,13 @@ import "fmt"
 // 2
 // 1
 func main() {
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+		if i == 5 {
+			panic("Panic")
+		}
+	}
+
 	defer fmt.Println(1)
 	defer fmt.Println(2)
 	fmt.Println(3)
