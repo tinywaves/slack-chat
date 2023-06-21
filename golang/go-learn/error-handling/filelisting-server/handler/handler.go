94923f67ad5a6173092ade9f7b3c729cdd0e3c0b
Denver Zheng <isdhzh@outlook.com>
2023-06-22 02:26:10 +0800

    <go> Errors

--- golang/go-learn/error-handling/filelisting-server/handler/handler.go
+++ golang/go-learn/error-handling/filelisting-server/handler/handler.go
@@ -4,10 +4,27 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
+const prefix = "/list/"
+
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+
+func (e userError) Message() string {
+	return string(e)
+}
+
 func FileListingHandler(writer http.ResponseWriter, request *http.Request) error {
-	path := request.URL.Path[len("/list/"):]
+	if strings.Index(request.URL.Path, prefix) != 0 {
+		return userError("path must start with " + prefix)
+	}
+
+	path := request.URL.Path[len(prefix):]
 
 	file, err := os.Open(path)
 	if err != nil {
--- golang/go-learn/error-handling/filelisting-server/web.go
+++ golang/go-learn/error-handling/filelisting-server/web.go
@@ -9,11 +9,32 @@ import (
 
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
+type userError interface {
+	error
+	Message() string
+}
+
 func errorWrapper(handler appHandler) func(writer http.ResponseWriter, request *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Default().Printf("Panic: %V", r)
+				http.Error(
+					writer,
+					http.StatusText(http.StatusInternalServerError),
+					http.StatusInternalServerError,
+				)
+			}
+		}()
 		err := handler(writer, request)
 		if err != nil {
 			log.Default().Println("Error handling request: ", err.Error())
+
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
+
 			httpCode := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -29,7 +50,7 @@ func errorWrapper(handler appHandler) func(writer http.ResponseWriter, request *
 }
 
 func main() {
-	http.HandleFunc("/list/", errorWrapper(handler.FileListingHandler))
+	http.HandleFunc("/", errorWrapper(handler.FileListingHandler))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		return
